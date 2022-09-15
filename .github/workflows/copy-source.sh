06226c350d694fd447484de6c802edd261ad48a1
Ngoc Nam Nguyen <nambrno1998@gmail.com>
2022-09-15 16:00:09 +0200

    Added workflow files

---
+++ .github/workflows/copy-source.sh
@@ -0,0 +1,18 @@
+cd iris-toolbox-knowledge-base
+
+if [[ -d source/ ]]; then
+    rm -rf source/
+fi
+
+mkdir source
+
+find . -name '*.md' -o -name '.pages' \
+    | xargs cp --parents -t ../source/
+
+cp ./source/README.md ./source/index.md
+
+cp -r ./extra ./source/extra
+
+tree -a ./source/ 
+
+cd ..
